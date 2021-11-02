4e8cd605544d37867a430260392cd094fd808e65
Catrina Alex <ovidiualex.catrina@gmail.com>
2021-11-02 22:53:54 +0200

    other commit

--- src/main/java/view/App.java
+++ src/main/java/view/App.java
@@ -10,9 +10,13 @@ public class App {
        /* login.play();*/
 
         Client c1=new Client(4,"Catrina","Alexandru","Str, Plevnei, nr. 1, Brasov","077668899","alextest2021");
+        Client c2=new Client(666,"Mutu","Adi","Str, Plevnei, nr. 1, Constanta","075568899","adiMt2021");
         ClientRepository cr= new ClientRepository();
 
-        cr.insert(c1);
+        //cr.insert(c1);
+        //cr.insert(c2);
+        //cr.updateAdresa("Mutu","Adi","sos. Nordului, nr. 78, Bucuresti");
+        //cr.delete(c2);
 
     }
 }
---
+++ trainticketingsystem_client.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: trainticketingsystem
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `client` (
+  `id_client` int NOT NULL AUTO_INCREMENT,
+  `nume` varchar(255) NOT NULL,
+  `prenume` varchar(255) NOT NULL,
+  `adresa` varchar(255) NOT NULL,
+  `nr_telefon` varchar(255) NOT NULL,
+  `parola` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_client`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (1,'Popescu','Ion','Bd. Eroilor, Bucuresti, sector2','0766982315','pion2021'),(2,'Ionescu','Ana','str I. C. Bratianu, nr. 13, Braila','0776328927','ananaionescu'),(3,'Catrina','Alexandru','str. C. Stere, nr. 65, Calafat','077668899','alextest2021');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-02 22:52:23
