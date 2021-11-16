ad7b92bb5cf69f100836a96f604d249c14ebbac1
Catrina Alex <ovidiualex.catrina@gmail.com>
2021-11-16 13:48:16 +0200

    other commit

--- src/main/java/repository/TrenRepository.java
+++ src/main/java/repository/TrenRepository.java
@@ -1,4 +1,48 @@
 package repository;
 
+import java.sql.*;
+
 public class TrenRepository {
+
+    private String Jdbc = "jdbc:mysql://localhost:3306/trainTicketingSystem";
+    private String username = "root";
+    private String password = "root";
+    private Connection connection = null;
+    private Statement statement = null;
+
+
+    public TrenRepository() {
+        try {
+            connection = DriverManager.getConnection(Jdbc, username, password);
+            statement = connection.createStatement();
+            //  System.out.println("S-a conectat");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void executeStatement(String execute){
+
+        try{
+            statement.execute(execute);
+
+        }catch (SQLException e){
+            System.out.println("Nu s-a realizat conectarea la baza de date"+ execute);
+        }
+    }
+
+    private ResultSet all(){
+
+        executeStatement("select * from tren");
+
+        try{
+            return statement.getResultSet();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
 }
