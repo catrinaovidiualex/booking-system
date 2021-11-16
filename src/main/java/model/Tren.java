892424f60ec71bc199e1b197b4508ae7ffe3dbf3
Catrina Alex <ovidiualex.catrina@gmail.com>
2021-11-16 12:31:03 +0200

    other commit

--- src/main/java/model/Tren.java
+++ src/main/java/model/Tren.java
@@ -1,4 +1,93 @@
 package model;
 
-public class Tren {
+public class Tren implements Comparable<Tren> {
+
+    private int id_tren;
+    private int numar_tren;
+    private String statiaPlecare;
+    private String statiaSosire;
+    private int id_vagon;
+
+    public Tren(int numar_tren, String statiaPlecare, String statiaSosire, int id_vagon) {
+
+        this.numar_tren = numar_tren;
+        this.statiaPlecare = statiaPlecare;
+        this.statiaSosire = statiaSosire;
+        this.id_vagon = id_vagon;
+    }
+
+    public void setId_tren(int id_tren){
+        this.id_tren=id_tren;
+    }
+    public int getId_tren(){
+        return this.id_tren;
+    }
+
+    public int getNumar_tren() {
+        return numar_tren;
+    }
+
+    public void setNumar_tren(int numar_tren) {
+        this.numar_tren = numar_tren;
+    }
+
+    public String getStatiaPlecare() {
+        return statiaPlecare;
+    }
+
+    public void setStatiaPlecare(String statiaPlecare) {
+        this.statiaPlecare = statiaPlecare;
+    }
+
+    public String getStatiaSosire() {
+        return statiaSosire;
+    }
+
+    public void setStatiaSosire(String statiaSosire) {
+        this.statiaSosire = statiaSosire;
+    }
+
+    public int getId_vagon() {
+        return id_vagon;
+    }
+
+    public void setId_vagon(int id_vagon) {
+        this.id_vagon = id_vagon;
+    }
+
+
+
+    @Override
+    public int compareTo(Tren tren) {
+        if (this.getId_tren()>tren.id_tren){
+            return 1;
+        }else if (this.getId_tren()<tren.id_tren){
+            return -1;
+        }
+
+        return 0;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Tren{" +
+                "id_tren=" + id_tren +
+                ", numar_tren=" + numar_tren +
+                ", statiaPlecare='" + statiaPlecare + '\'' +
+                ", statiaSosire='" + statiaSosire + '\'' +
+                ", id_vagon=" + id_vagon +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object trn) {
+        Tren tren = (Tren) trn;
+        return this.getId_tren()==tren.getId_tren();
+
+    }
+
+
 }
+
+
