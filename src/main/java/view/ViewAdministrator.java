ef9b70250fad225d8920ff06901a8c53f22dcc0a
Catrina Alex <ovidiualex.catrina@gmail.com>
2021-12-08 21:00:34 +0200

    other commit

--- src/main/java/view/ViewAdministrator.java
+++ src/main/java/view/ViewAdministrator.java
@@ -1,5 +1,19 @@
 package view;
 
+import controller.*;
+import model.Angajat;
+
+import java.util.Scanner;
+
 public class ViewAdministrator {
+
+    private Angajat angajat;
+    private AngajatController angajati;
+    private TrenController trenuri;
+    private ClientController clienti;
+    private FacturaController facturi;
+    private RezervareController rezervari;
+    private VagonController vagoane;
+    private Scanner scanner;
 
 }
