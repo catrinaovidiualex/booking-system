389348da95eaf7081ca42861c6d519f3ddb89d6c
Catrina Alex <ovidiualex.catrina@gmail.com>
2021-12-21 22:07:38 +0200

    other commit

--- src/main/java/view/ViewClient.java
+++ src/main/java/view/ViewClient.java
@@ -1,4 +1,27 @@
 package view;
 
+import controller.*;
+import model.Client;
+
+import java.util.Scanner;
+
 public class ViewClient {
+
+    private Client client;
+    private ClientController clienti;
+    private FacturaController facturi;
+    private RezervareController rezervari;
+    private TrenController trenuri;
+    private VagonController vagoane;
+    private Scanner scanner;
+
+    public ViewClient(Client client) {
+        this.client = client;
+        this.clienti = new ClientController();
+        this.facturi = new FacturaController();
+        this.rezervari = new RezervareController();
+        this.trenuri = new TrenController();
+        this.vagoane = new VagonController();
+        this.scanner = scanner;
+    }
 }
