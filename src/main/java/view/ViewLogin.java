c3fcbe63abc7d62d371a8157cb41ec62a2e56335
Catrina Alex <ovidiualex.catrina@gmail.com>
2021-12-28 16:13:08 +0200

    other commit

--- src/main/java/view/ViewLogin.java
+++ src/main/java/view/ViewLogin.java
@@ -1,4 +1,27 @@
 package view;
+import controller.AngajatController;
+import controller.ClientController;
+import model.Angajat;
+import model.Client;
 
+import javax.swing.text.View;
+import java.util.Scanner;
 public class ViewLogin {
+    private Scanner scanner;
+    private AngajatController angajati;
+    private ClientController clienti;
+
+    public ViewLogin() {
+        this.scanner = new Scanner(System.in);
+        this.angajati = new AngajatController();
+        this.clienti = new ClientController();
+    }
+
+    private String meniu(){
+        String text="";
+        text+="Apasati tasta 0 pentru a va deconecta\n";
+        text+="Apasati tasta 1 pentru a va loga\n";
+        return text;
+    }
+
 }
