843f46d913483d8afd9da30cf11d2a8eb77c5587
Catrina Alex <ovidiualex.catrina@gmail.com>
2022-01-04 21:37:23 +0200

    other commit

--- src/main/java/view/ViewLogin.java
+++ src/main/java/view/ViewLogin.java
@@ -5,6 +5,7 @@ import model.Angajat;
 import model.Client;
 
 import javax.swing.text.View;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class ViewLogin {
     private Scanner scanner;
@@ -24,40 +25,66 @@ public class ViewLogin {
         return text;
     }
 
-    private void login(){
+    private void login() {
         System.out.println("Introduceti statusul dumneavoastra: client sau administrator");
-        String status=scanner.nextLine();
-        String nume="";
-        String parola="";
+        String status = scanner.nextLine();
+        String nume = "";
+        String prenume = "";
+        String parola = "";
 
-        if(status.equals("admin")==false && status.equals("client")==false){
+        if (status.equals("admin") == false && status.equals("client") == false) {
             System.out.println("Nu ati introdus un status valid!");
 
-        }
-        else{
+        } else {
             System.out.println("Introduceti numele dumeavoastra:");
-            nume=scanner.nextLine();
+            nume = scanner.nextLine();
+            System.out.println("Introduceti prenumele dumeavoastra:");
+            prenume = scanner.nextLine();
             System.out.println("Introduceti parola dumneavoastra:");
-            parola=scanner.nextLine();
+            parola = scanner.nextLine();
 
-        }
-        if (status.equals("admin")) {
 
-            if (angajati.parola(nume) == null || !angajati.parola(nume).equals(parola)) {
-                System.out.println("numele de utilizator sau parola este incorecta");
-            } else {
-                Angajat a = angajati.angajat(nume);
-                ViewAdministrator administrator = new ViewAdministrator(a);
-                administrator.play();
-            }
-        }
-            else if (status.equals("client")){
+            if (status.equals("admin")) {
 
+                if (angajati.parola(nume) == null || !angajati.parola(nume).equals(parola)) {
+                    System.out.println("numele de utilizator sau parola este incorecta");
+                } else {
+                    Angajat a = angajati.angajat(nume);
+                    ViewAdministrator administrator = new ViewAdministrator(a);
+                    administrator.play();
+                }
+            } else if (status.equals("client")) {
+                if (clienti.parola(nume, prenume) == null || !clienti.parola(nume, prenume).equals(parola)) {
+                    System.out.println("nume, prenumele sau parola sunt incorecte");
+                } else {
+                    Client c = clienti.client(nume, prenume);
+                    ViewClient client = new ViewClient(c);
+                    client.play();
+                }
 
 
             }
         }
     }
+
+    public void play(){
+        System.out.println(meniu());
+        boolean run=true;
+        while(run){
+            int alegere=Integer.parseInt(scanner.nextLine());
+            switch(alegere){
+                case 0:
+                    run=false;
+                    break;
+                case 1:
+                    login();
+                    break;
+                default:
+                    System.out.println(meniu());
+            }
+        }
+    }
+}
 
 
 
