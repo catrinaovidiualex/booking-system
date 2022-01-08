507691935328a7ff4ae6f9ff13eb96a357a9c12a
Catrina Alex <ovidiualex.catrina@gmail.com>
2022-01-09 01:24:58 +0200

    other commit

--- src/main/java/view/ViewAdministrator.java
+++ src/main/java/view/ViewAdministrator.java
@@ -61,12 +61,55 @@ public class ViewAdministrator {
                 case 1:
                     facturi.afisare();
                     break;
-
-
-
+                case 2:
+                    insertFactura();
+                    break;
+                case 3:
+                    deleteFactura();
+                    break;
+                case 4:
+                    modificarePretBiletPeFactura();
+                    break;
+                default:
+                    System.out.println(meniuFactura());
+                    break;
             }
 
         }
+
+    }
+
+
+    public void play(){
+
+    }
+
+    //insert statements
+    private void insertFactura(){
+        System.out.println("Introduceti id-ul clientului\n");
+        int idClient=Integer.parseInt(scanner.nextLine());
+        System.out.println("Introduceti pretul biletului\n");
+        int pretBilet=Integer.parseInt(scanner.nextLine());
+
+
+        Factura factura=new Factura(idClient,pretBilet);
+        facturi.insert(factura);
+
+    }
+    private void deleteFactura(){
+        System.out.println("Introduceti id-ul facturii pe care doriti sa o stergeti:");
+        int idFactura=Integer.parseInt(scanner.nextLine());
+
+        facturi.deleteFactura(idFactura);
+    }
+    private void modificarePretBiletPeFactura(){
+
+        System.out.println("Introduceti id-ul facturii careia doriti sa ii schimbati pretul per bilet");
+        int idFactura=Integer.parseInt(scanner.nextLine());
+        System.out.println("Introduceti noul pret:");
+        int pretNou=Integer.parseInt(scanner.nextLine());
+
+        facturi.updatePretBilet(idFactura,pretNou);
 
     }
 
